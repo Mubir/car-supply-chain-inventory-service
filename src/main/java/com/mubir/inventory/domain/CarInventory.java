517e2bbf649430f4a0f083014bb03f551913db70
Munir <info.mubir@gmail.com>
2020-08-05 15:34:00 +0900

    entity added

--- src/main/java/com/mubir/inventory/domain/CarInventory.java
+++ src/main/java/com/mubir/inventory/domain/CarInventory.java
@@ -1,2 +1,28 @@
-package com.mubir.inventory.domain;public class CarInventory {
+package com.mubir.inventory.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import java.sql.Timestamp;
+import java.util.UUID;
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class CarInventory extends BaseEntity{
+    @Builder
+    public CarInventory(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate,
+                        UUID beerId, String upc, Integer quantityOnHand) {
+        super(id, version, createdDate, lastModifiedDate);
+        this.beerId = beerId;
+        this.upc = upc;
+        this.quantityOnHand = quantityOnHand;
+    }
+
+    private UUID beerId;
+    private String upc;
+    private Integer quantityOnHand = 0;
 }
