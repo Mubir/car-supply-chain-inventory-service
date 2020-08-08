478346b24bdb0a3b72a0cc64b877cb2eaf108f05
Munir <info.mubir@gmail.com>
2020-08-08 22:48:33 +0900

    mysql error resolved

--- src/main/scripts/mysql-init.sql
+++ src/main/scripts/mysql-init.sql
@@ -1,7 +1,13 @@
-DROP DATABASE IF EXISTS carsinventoryervice;
+DROP DATABASE IF EXISTS carinventoryservice;
+
 DROP USER IF EXISTS `car_inventory_service`@`%`;
+
 CREATE DATABASE IF NOT EXISTS carinventoryservice CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 CREATE USER IF NOT EXISTS `car_inventory_service`@`%` IDENTIFIED WITH mysql_native_password BY '12345678';
+
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
-    CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `carinventoryservice`.* TO `car_inventory_service`@`%`;
+
+CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `carinventoryservice`.* TO `car_inventory_service`@`%`;
+
 FLUSH PRIVILEGES;
